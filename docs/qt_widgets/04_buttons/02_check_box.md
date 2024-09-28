4283894149a79f6acedecb8e07fe575aaeed49c7
Joe <anonimista@outlook.com>
2024-09-28 19:51:03 +0200

    checkbox example

--- docs/qt_widgets/04_buttons/02_check_box.md
+++ docs/qt_widgets/04_buttons/02_check_box.md
@@ -10,3 +10,9 @@ parent: 04 Buttons
 ```python
 {% include src/qtwidgetsexamples/04_buttons/02_check_box.py %}
 ```
+
+`QCheckBox` is  a widget that has a graphical check box and a label. it can be checked, unchecked and, if you set its `tristate` property to `True`, partially checked. Checkboxes are commonly used to represent options that are __not__ mutually exclusive - more than one checkbox in a group can be checked at the same time. To use `QCheckBox` in your application
+
+1. Create a `QCheckBox` object passing the text to be displayed to the constructor. In the example we create three checkboxes, one for each popular operating system and add each of them to the window's layout
+
+2. Create the slot to process checkboxes' `stateChanged
--- src/qtwidgetsexamples/04_buttons/02_check_box.py
+++ src/qtwidgetsexamples/04_buttons/02_check_box.py
@@ -1,9 +1,11 @@
 # A QCheckBox is an option button that can be 
-# switched on (checked) or off (unchecked). 
+# checked, unchecked or partially checked. 
 
-from PySide6.QtWidgets import (QApplication, 
-    QWidget, QVBoxLayout, QCheckBox, QLabel)
 import sys
+
+from PySide6.QtCore import Slot, Qt
+from PySide6.QtWidgets import (QApplication,
+    QWidget, QVBoxLayout, QCheckBox, QLabel)
 
 
 class Window(QWidget):
@@ -12,47 +14,48 @@ class Window(QWidget):
 
         super().__init__()
         
+        self.resize(300, 200)
         layout = QVBoxLayout()
         self.setLayout(layout)
         
         # 1 - Create the checkboxes and add them to the layout
         
         self.windows_checkbox = QCheckBox('Windows')
+        self.windows_checkbox.setObjectName('Windows')
+
         self.linux_checkbox = QCheckBox('Linux')
-        self.mac_checkbox = QCheckBox('Mac')
+        self.linux_checkbox.setObjectName('Linux')
         
+        self.macos_checkbox = QCheckBox('macOS')
+        self.macos_checkbox.setObjectName('macOS')
+        self.macos_checkbox.setTristate(True)
+
         self.label = QLabel()
         
         layout.addWidget(self.windows_checkbox)
         layout.addWidget(self.linux_checkbox)
-        layout.addWidget(self.mac_checkbox)
+        layout.addWidget(self.macos_checkbox)
+        
         layout.addWidget(self.label)
         
-        # 3 - For each checkbox connect QCheckBox.stateChanged signal
-        #     with the slot. stateChanged is emitted when a checkbox
-        #     is checked or unchecked.
+        # 3 - For each checkbox connect QCheckBox.checkStateChanged signal
+        #     with the slot. checkStateChanged is emitted when a checkbox
+        #     state changes.
         
-        self.windows_checkbox.stateChanged.connect(self.on_state_changed)
-        self.linux_checkbox.stateChanged.connect(self.on_state_changed)
-        self.mac_checkbox.stateChanged.connect(self.on_state_changed)
+        self.windows_checkbox.checkStateChanged.connect(self.on_state_changed)
+        self.linux_checkbox.checkStateChanged.connect(self.on_state_changed)
+        self.macos_checkbox.checkStateChanged.connect(self.on_state_changed)
     
-    # 2 - Create the slot. You are interested in
-    #     QCheckBox.isChecked() which can be true or false
+    # 2 - Create the slot. Mark it with the @Slot() decorator
+    #     If you don't import PySide6.QtCore.Qt above
+    #     PySide6 will complain: TypeError: Cannot call meta function ...
+    
+    @Slot()
+    def on_state_changed(self, state):
+        sender_name = self.sender().objectName()
+        self.label.setText(f'{sender_name} {state}')
         
-    def on_state_changed(self):
         
-        label_text =  ''
-        
-        if self.windows_checkbox.isChecked():
-            label_text += 'Windows\n'
-            
-        if self.linux_checkbox.isChecked():
-            label_text += 'Linux\n'
-            
-        if self.mac_checkbox.isChecked():
-            label_text += 'Mac\n'
-            
-        self.label.setText(label_text)
 
 
 if __name__ == '__main__':
