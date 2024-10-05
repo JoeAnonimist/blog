ccef1569d3ff3b434b1eef491c249390ddda1d56
Joe <anonimista@outlook.com>
2024-10-05 18:20:23 +0200

    QLineEdit example

--- docs/qt_widgets/05_input_widgets/01_line_edit.md
+++ docs/qt_widgets/05_input_widgets/01_line_edit.md
@@ -10,3 +10,10 @@ parent: 05 Input Widgets
 ```python
 {% include src/qtwidgetsexamples/05_input_widgets/01_line_edit.py %}
 ```
+`QLineEdit` lets you edit a line of plain text. You can enter, `copy()`, `cut()`, `paste()`, `undo()`, `redo()` or `selectAll()` the text using the standard keyboard shortcuts or the built-in context menu. You can constrain the text content using `QValidator`s or input masks. This example features a `QRegularExpressionValidator` that allows only alphabetic input. You can turn it into a password input field setting its `echoMode` property to `EchoMode.Password`. To use `QLineEdit` in your application
+
+1. Create a `QLineEdit` instance and add it to a layout.
+
+2. Create the slots to handle `QLineEdit` signals. The `textChanged` signal is emitted whenever the line edit text changes. The `editingFinished` signal is emitted when the Enter key is pressed or the line edit widget loses focus. The `inputRejected` signal is emitted when the user tries to enter invalid input. In this example all three slots set the label text based on the emitted signal.
+
+3. Connect the signals with the slot methods using the standard PySide6 notation.
--- src/qtwidgetsexamples/05_input_widgets/01_line_edit.py
+++ src/qtwidgetsexamples/05_input_widgets/01_line_edit.py
@@ -1,8 +1,11 @@
 # The QLineEdit widget is a one-line text editor. 
 
-from PySide6.QtWidgets import (QApplication, 
-    QWidget, QVBoxLayout, QLineEdit, QLabel)
 import sys
+
+from PySide6.QtCore import Slot
+from PySide6.QtGui import QRegularExpressionValidator
+from PySide6.QtWidgets import (QApplication,
+    QWidget, QVBoxLayout, QLineEdit, QLabel)
 
 
 class Window(QWidget):
@@ -17,21 +20,34 @@ class Window(QWidget):
         # 1 - Create a line edit widget instance
         
         self.line_edit = QLineEdit()
+        validator = QRegularExpressionValidator('^[a-zA-Z]*$')
+        self.line_edit.setValidator(validator)
+        
         self.label = QLabel()
         
-        # 3 - Connect the signal and the slot
+        # 3 - Connect the signals with the slots
         
         self.line_edit.textChanged.connect(self.on_text_changed)
+        self.line_edit.editingFinished.connect(self.on_editing_finished)
+        self.line_edit.inputRejected.connect(self.on_input_rejected)
         
         layout.addWidget(self.line_edit)
         layout.addWidget(self.label)
-        
-        
-    # 2 - Create a method to handle 
-    #     line edit text changed events.
-        
+
+    # 2 - Create methods to handle line edit events. 
+    
+    @Slot()
     def on_text_changed(self):
         self.label.setText(self.line_edit.text())
+    
+    @Slot()
+    def on_editing_finished(self):
+        self.label.setText(
+            f'Editing finished: {self.line_edit.text()}')
+        
+    @Slot()
+    def on_input_rejected(self):
+        self.label.setText('Only alphanumeric characters allowed')
 
 
 if __name__ == '__main__':
