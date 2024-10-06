d6a09ffd7a66b50a0335a855a969661e778c9522
Joe <anonimista@outlook.com>
2024-10-06 14:56:49 +0200

    spinbox example

--- src/qtwidgetsexamples/05_input_widgets/03_spin_box.py
+++ src/qtwidgetsexamples/05_input_widgets/03_spin_box.py
@@ -2,7 +2,7 @@
 # The QDial class provides a rounded range control
 # (like a speedometer or potentiometer).
 #
-# Access their curent value using their value() property
+# Access their current value using their value() property
 
 from PySide6.QtWidgets import (QApplication, 
     QWidget, QVBoxLayout, QSpinBox, QDial, QLabel)
@@ -20,37 +20,36 @@ class Window(QWidget):
         
         # 1 - Create the widgets
         
-        spin_box = QSpinBox()
-        spin_box.setRange(0, 100)
-        spin_box.setSingleStep(5)
+        self.spinbox = QSpinBox()
+        self.spinbox.setRange(0, 100)
+        self.spinbox.setSingleStep(5)
         
-        dial = QDial()
-        dial.setRange(0, 100)
+        self.dial = QDial()
+        self.dial.setRange(0, 100)
         
         self.label = QLabel()
         
-        # You can pass additional arguments to Qt slots
-        # using Python lambda
-        
-        spin_box.valueChanged.connect(
-            lambda: self.on_value_changed(spin_box, dial))
+        self.spinbox.valueChanged.connect(
+            self.on_spinbox_value_changed)
             
-        dial.valueChanged.connect(
-            lambda: self.on_value_changed(dial, spin_box))
+        self.dial.valueChanged.connect(
+            self.on_dial_value_changed)
         
         # 2 - Add widgets to the layout
         
-        layout.addWidget(spin_box)
-        layout.addWidget(dial)
+        layout.addWidget(self.spinbox)
+        layout.addWidget(self.dial)
         layout.addWidget(self.label)
 
     # 3 - Get widget value. It's an integer.
         
-    def on_value_changed(self, sender, target):
+    def on_spinbox_value_changed(self, i):
+        self.dial.setValue(i)
+        self.label.setText(str(i))
         
-        value = sender.value()
-        target.setValue(value)
-        self.label.setText(str(value))
+    def on_dial_value_changed(self, i):
+        self.spinbox.setValue(i)
+        self.label.setText(str(i))
 
 
 if __name__ == '__main__':
