de43a3119f3cc4103de206cd30807e23716e73a2
JoeAnonimist <anonimista@outlook.com>
2024-10-15 13:56:11 +0200

    hello world

--- src/qtquickexamples/01_template/01_hello_world.py
+++ src/qtquickexamples/01_template/01_hello_world.py
@@ -6,20 +6,20 @@ from PySide6.QtQml import QQmlApplicationEngine
 
 if __name__ == '__main__':
     
-    # 3. Create a QGuiApplication instance
+    # 1. Create a QGuiApplication instance
 
     app = QGuiApplication(sys.argv)
     
-    # 4. Create a QQmlApplicationEngine instance and connect
+    # 2. Create a QQmlApplicationEngine instance and connect
     #    its quit signal with QGuiApplication.quit()
     
     engine = QQmlApplicationEngine()
     engine.quit.connect(app.quit)
     
-    # 5. Load the Qml file
+    # 3. Load the Qml file
     
     engine.load('01_hello_world.qml')
     
-    # 6.  Start the main event loop
+    # 4.  Start the main event loop
 
     sys.exit(app.exec())
