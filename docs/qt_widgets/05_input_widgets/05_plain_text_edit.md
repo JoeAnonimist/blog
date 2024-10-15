95eb3f1d8ae49b36c90a58530d70a4c3b463a8f7
Uros.Calakovic <anonimista@outlook.com>
2024-10-15 09:46:53 +0200

    QPLainTextEdit example

--- docs/qt_widgets/05_input_widgets/05_plain_text_edit.md
+++ docs/qt_widgets/05_input_widgets/05_plain_text_edit.md
@@ -10,3 +10,13 @@ parent: 05 Input Widgets
 ```python
 {% include src/qtwidgetsexamples/05_input_widgets/05_plain_text_edit.py %}
 ```
+
+`QPlainTextEdit` is a multi-line text edit widget. It is used to edit plain text so you can't use it to style text the way you can in `QTextEdit` ie. its formatting abilities are limited. You can use `QSyntaxHighlighter` with it so it is possible to make a simple programming editor with it. If you set `QPlainTextEdit.setEnabled(False)` you can use this widget to display multi-line text that doesn't need to be edited. To use `QPlainTextEdit` in your application
+
+1. Create an instance of `QPLainTextEdit` and optionally set its font using `QFont`. In the example we set the font to Droid Sans Mono, set the font size in pixels and add a style hint to it to use a monospace font if Droid Sans Mono is not installed on the user's system. We also create two labels, one for displaying total character count and another to display cursor position in the `QPLainTextEdit`.
+
+2. Write the slot methods. The first slot the first label's text to the total number of entered characters every time the text in the `QPLainTextEdit` changes. The second slot sets the second label's text to the current cursor position in the `QPLainTextEdit` when the cursor is moved.
+
+  Just like with `QTextEdit` you can access `QPLainTextEdit`'s underlying `QTextDocument` using `QPlainTextEdit.document()` and its `QTextCursor` using `QPLainTextEdit.textCursor()`
+
+3. Add the `QPlainTextEdit` and the two labels to the window's layout and connect the slots to the `QPlainTextEdit`'s `textChanged` and `cursorPositionChanged` signals.
--- src/qtwidgetsexamples/05_input_widgets/05_plain_text_edit.py
+++ src/qtwidgetsexamples/05_input_widgets/05_plain_text_edit.py
@@ -8,9 +8,12 @@
 # You can also access the whole text 
 # using QPLainTextEdit.plainText().
 
-from PySide6.QtWidgets import (QApplication, 
-    QWidget, QVBoxLayout, QPlainTextEdit, QLabel)
 import sys
+
+from PySide6.QtCore import Slot
+from PySide6.QtGui import QFont
+from PySide6.QtWidgets import (QApplication,
+    QWidget, QVBoxLayout, QPlainTextEdit, QLabel)
 
 
 class Window(QWidget):
@@ -23,15 +26,22 @@ class Window(QWidget):
         self.setLayout(layout)
         
         # 1 - Create the plain text edit widget
+        #     and set its font and wrap mode.
         
         self.plain_text_edit = QPlainTextEdit()
+        self.plain_text_edit.setLineWrapMode(
+            QPlainTextEdit.LineWrapMode.NoWrap)
+        self.font = QFont('Droid Sans Mono')
+        self.font.setPixelSize(12)
+        self.plain_text_edit.setFont(self.font)
+        self.font.setStyleHint(QFont.StyleHint.Monospace)
         
         # These are for displaying document stats.
         
         self.label_char_count = QLabel()
         self.label_cursor_position = QLabel()
         
-        # 2 - Add the plain text edit to the layout
+        # 3 - Add the QPlainTextEdit to the layout
         
         layout.addWidget(self.plain_text_edit)
         layout.addWidget(self.label_char_count)
@@ -41,22 +51,26 @@ class Window(QWidget):
         self.plain_text_edit.cursorPositionChanged.connect(
             self.on_cursor_position_changed)
     
-    # 3 - Get the underlying QTextDocument properties
+    # 2 - Get the underlying QTextDocument properties
     
     # This slot handles text changed events
-            
+    
+    @Slot()        
     def on_text_changed(self):
+        
         char_count = self.plain_text_edit.document().characterCount()
-        self.label_char_count.setText('Char count: ' + str(char_count))
+        self.label_char_count.setText(f'Char count: {str(char_count)}')
     
     # This one handles cursor position changes
     
+    @Slot()
     def on_cursor_position_changed(self):
-        cursor = self.plain_text_edit.textCursor()
-        self.label_cursor_position.setText(
-            'x: ' + str(cursor.block().blockNumber()) + 
-            ', y: ' + str(cursor.positionInBlock()))
         
+        cursor = self.plain_text_edit.textCursor()
+        x = str(cursor.block().blockNumber() + 1)
+        y = str(cursor.positionInBlock() + 1)
+        
+        self.label_cursor_position.setText(f'x: {x} y: {y}')
 
 
 if __name__ == '__main__':
