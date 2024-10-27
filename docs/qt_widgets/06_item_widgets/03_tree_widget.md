8f1a6702f0e4aa492617c8d0500f56ff26655db1
Joe <anonimista@outlook.com>
2024-10-27 16:37:01 +0100

    QTreeWidget

--- docs/qt_widgets/06_item_widgets/03_tree_widget.md
+++ docs/qt_widgets/06_item_widgets/03_tree_widget.md
@@ -13,17 +13,8 @@ parent: 06 Item Widgets
 
 The `QTreeWidget` provides a standard tree widget. It uses a predefined item model - each tree node is represented with a `QTreeWidgetItem` instance. To use `QTreeWidget in your application
 
-1. Create a `QTreeWidget` object
+1. Create a `QTreeWidget` object. It has only one constructor that optionally accepts the tree widget's parent widget but we don't set it here because adding a widget to a layout automatically sets the widget's parent.
 
-```mermaid
-flowchart LR;
-A(A node)-- Event 1 ---B(B node);
-A(A node)-- Event 2 ---C;
-B-->D;
-C-->D;
-D==>E;
-E==>F;
-E==>G;
-F-->H;
-G-->H;
-```
+2. Create `QTreeWidgetItem` objects and add them to the tree widget. Each tree node is represented by a `QTreeWidgetItem` object. If you set the `QTreeWidget` as its parent, like we do with the `apes` `QTreeWidgetItem` it becomes a root/top level item. If you set another `QTreeWidgetItem` as the parent that item becomes the parent and you use this parent-child relationship to form a tree structure that is displayed it the tree widget. We also use string lists like `['Gibbons']` to set the text to be displayed for each `QTreeWidgetItem`. `QTreeWidget` has one column by default but you can add more if you need.
+
+3. Add necessary slots, connect them with the appropriate `QTreeWidget` signals and add the tree widget to the window layout. In the example we set a label's text to the current item text each time the tree widget current item changes.
--- src/qtwidgetsexamples/06_item_widgets/03_tree_widget.py
+++ src/qtwidgetsexamples/06_item_widgets/03_tree_widget.py
@@ -23,30 +23,25 @@ class Window(QWidget):
         # 2 - Create the tree widget items and add them 
         #     to the tree widget
         #
-        # QTreeWidgetItem.ItemType.Type value is 0
-        # You can use 0 instead of the enum value
-        # but there's no guarantee it will stay 0 in the future.
-        #
         # apes parent is self.tree_widget which means 
         # apes is the root item
         #
         # ['Apes'] is the item text. It's a list with one element.
         # That's because a tree widget has one column by default.
-        apes = QTreeWidgetItem(self.tree_widget, ['Apes'],
-            QTreeWidgetItem.ItemType.Type)
+        apes = QTreeWidgetItem(self.tree_widget, ['Apes'])
         
         # Add other items to the tree. The pattern is the same
-        # as for the root item: (parent, text, type)
+        # as for the root item: (parent, text)
         # There are several other QTreeWidgetItem 
         # constructors as well.
-        gibbons = QTreeWidgetItem(apes, ['Gibbons'], 0)
-        hominidae = QTreeWidgetItem(apes, ['Hominidae'], 0)
-        ponginae = QTreeWidgetItem(hominidae, ['Orangutans'], 0)
-        homininae = QTreeWidgetItem(hominidae, ['Homininae'], 0)
-        gorillinae = QTreeWidgetItem(homininae, ['Gorillas'], 0)
-        hominini = QTreeWidgetItem(homininae, ['Hominini'], 0)
-        panina = QTreeWidgetItem(hominini, ['Chimpanzees'], 0)
-        hominina = QTreeWidgetItem(hominini, ['Hominina'], 0)
+        gibbons = QTreeWidgetItem(apes, ['Gibbons'])
+        hominidae = QTreeWidgetItem(apes, ['Hominidae'])
+        ponginae = QTreeWidgetItem(hominidae, ['Orangutans'])
+        homininae = QTreeWidgetItem(hominidae, ['Homininae'])
+        gorillinae = QTreeWidgetItem(homininae, ['Gorillas'])
+        hominini = QTreeWidgetItem(homininae, ['Hominini'])
+        panina = QTreeWidgetItem(hominini, ['Chimpanzees'])
+        hominina = QTreeWidgetItem(hominini, ['Hominina'])
         
         # Add some event handling just for show. 
         self.tree_widget.currentItemChanged.connect(
