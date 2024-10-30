d91f0bcfdb526c5d1f8a6b2195b6c806921c92c6
JoeAnonimist <anonimista@outlook.com>
2024-10-30 09:50:40 +0100

    QGroupBox

--- src/qtwidgetsexamples/08_containers/01_group_box.py
+++ src/qtwidgetsexamples/08_containers/01_group_box.py
@@ -13,11 +13,6 @@ class Window(QWidget):
     def __init__(self):
         
         super().__init__()
-        self.initUi()
-        
-        self.show()
-        
-    def initUi(self):
 
         layout = QHBoxLayout()
 
@@ -25,43 +20,52 @@ class Window(QWidget):
         self.label.setFixedWidth(80)
         
         # 1 - Create the group box
+        #     and add a layout to it. You can't 
+        #     add widgets directly to the group box.
         
-        groupbox = QGroupBox()
-        groupbox.setTitle('Group box')
-        
-        # 2 - Set the group box layout
-        #     You can't add widgets directly to the group box.
-        
-        groupbox_layout = QVBoxLayout()
-        groupbox.setLayout(groupbox_layout)
+        self.groupbox = QGroupBox()
+        self.groupbox.setTitle('Group box')
 
-        # 3 - Add widgets to the layout.
+        groupbox_layout = QVBoxLayout()
+        self.groupbox.setLayout(groupbox_layout)
+
+        # 2 - Add widgets to the layout.
 
         self.radiobutton_1 = QRadioButton('Option 1')
-        self.radiobutton_2 = QRadioButton('Option 2')
+        self.radiobutton_2 = QRadioButton('Set checkable')
+        self.radiobutton_3 = QRadioButton('Set non-checkable')
 
         groupbox_layout.addWidget(self.radiobutton_1)
         groupbox_layout.addWidget(self.radiobutton_2)
+        groupbox_layout.addWidget(self.radiobutton_3)
         
-        # Handle radio button toggle signals
+        # 4- Connect child widget signals with the slot
         
         self.radiobutton_1.toggled.connect(self.on_toggled)
         self.radiobutton_2.toggled.connect(self.on_toggled)
+        self.radiobutton_3.toggled.connect(self.on_toggled)
+        
+        self.radiobutton_1.setChecked(True)
 
-        layout.addWidget(groupbox)
+        layout.addWidget(self.groupbox)
         layout.addWidget(self.label)
         
         self.setLayout(layout)
 
-    # The slot method. If there are multiple radio buttons
-    # in a group box only one can be checked.
-    # Not so with check boxes.
+    # 3 - Add slot method. If there are 
+    #     multiple radio buttons in a group box
+    #     only one can be checked, unlike checkboxes.
     
     def on_toggled(self):
+
         if self.radiobutton_1.isChecked():
             self.label.setText(self.radiobutton_1.text())
-        else:
+        elif self.radiobutton_2.isChecked():
             self.label.setText(self.radiobutton_2.text())
+            self.groupbox.setCheckable(True)
+        else:
+            self.label.setText(self.radiobutton_3.text())
+            self.groupbox.setCheckable(False)
 
 
 if __name__ == '__main__':
