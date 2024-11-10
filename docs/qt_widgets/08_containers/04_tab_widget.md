0c6e38a0104a4787686feb511db6a65ec37cfa86
Joe <anonimista@outlook.com>
2024-11-10 15:31:29 +0100

    qtabwidget

--- docs/qt_widgets/08_containers/04_tab_widget.md
+++ docs/qt_widgets/08_containers/04_tab_widget.md
@@ -12,3 +12,13 @@ parent: 08 Containers
 ```python
 {% include src/qtwidgetsexamples/08_containers/04_tab_widget.py %}
 ```
+
+`QTabWidget` is a tabbed container widget - when you click on a tab its associated page is shown. To use it in your application
+
+1. Create a `QTabWidget` object. 
+
+2. Create its intended child widgets. Each page contains a single widget but that widget, in turn, can be a `QWidget` or a container class which allows you to pack multiple children into a `QTabWidget` page.
+
+3. Use `QTabWidget.addTab()` to add the child widgets to the tab widget object. In the example we have two `QWidget`s and add each to the tab widget. The first `QWidget` has three check boxes as its children and the second `QWidget`s  children are three radio buttons.
+
+The tab indexes start with zero so the first tab has the index of `0` and the second has the index of `1`. Each tab has a label (`Styles` and `Margins` in the example). You can change the tab position (`North`, `South`, `West` and `East`) and shape (`Rounded` and `Triangular`). 
--- src/qtwidgetsexamples/08_containers/04_tab_widget.py
+++ src/qtwidgetsexamples/08_containers/04_tab_widget.py
@@ -3,28 +3,28 @@
 
 import sys
 
-from PySide6.QtWidgets import (QApplication,
-    QWidget, QVBoxLayout, QTabWidget, QRadioButton, QCheckBox)
+from PySide6.QtWidgets import (QApplication, QWidget,
+    QVBoxLayout, QTabWidget, QRadioButton, QCheckBox)
 
 
 class Window(QWidget):
-    
+
     def __init__(self):
 
         super().__init__()
-        
+
         layout = QVBoxLayout()
         self.setLayout(layout)
-        
+
         # 1 - Create the tab widget
-        
+
         self.tab_widget = QTabWidget()
-        
+
         # 2 - Create some widgets
         #     A tab contains only one widget
         #     but that widget can be a QWidget instance
         #     or some other container.
-        
+
         # Tab 0 widgets:
 
         styles = QWidget()
@@ -33,18 +33,18 @@ class Window(QWidget):
         styles_layout.addWidget(QCheckBox('Paragraph'))
         styles_layout.addWidget(QCheckBox('List'))
         styles.setLayout(styles_layout)
-        
+
         # Tab 1 widgets
-        
+
         margins = QWidget()
         margins_layout = QVBoxLayout()
         margins_layout.addWidget(QRadioButton('Normal'))
         margins_layout.addWidget(QRadioButton('Wide'))
         margins_layout.addWidget(QRadioButton('Narrow'))
         margins.setLayout(margins_layout)
-        
+
         # 3 - Add tabs to the widget
-        
+
         self.tab_widget.addTab(styles, 'Styles')
         self.tab_widget.addTab(margins, 'Margins')
 
