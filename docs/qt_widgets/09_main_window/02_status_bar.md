f4680f1f89f4c89fd521aebe023aec965969f2ce
Joe <anonimista@outlook.com>
2024-11-12 18:00:39 +0100

    qstatusbar

--- docs/qt_widgets/09_main_window/02_status_bar.md
+++ docs/qt_widgets/09_main_window/02_status_bar.md
@@ -13,3 +13,16 @@ parent: 09 Main Window
 {% include src/qtwidgetsexamples/09_main_window/02_status_bar.py %}
 ```
 
+The `QStatusBar` class represents the main window status bar. It can display three kinds of information:
+
+- Temporary, eg. short help messages,
+- Normal, eg. row and column number
+- Permanent, eg. the Caps Lock status
+
+To use it in your application
+
+1. Create the widgets to hold the desired status information. In the example we create two labels, one to display the cursor row and column and the other to display total character count.
+
+2. Add the widgets to the status bar. You access the status bar using the `QMainWindow.statusBar()` method and add widgets to it with either `addWidget()` or `addPermanentWidget()`. The widgets added with the former method are placed on the left and the widgets added with the latter are placed on the right hand side of the status bar.
+
+3. Use the appropriate signals to display the status information. In the example we display three pieces of information: the total character count, which displayed permanently, the current cursor position (normal) and a message to notify the user when there is selected text available for copying (temporary). The temporary message, "Copy available!" is displayed for two seconds and hides the normal status widgets but does not hide the permanent status information.
--- src/qtwidgetsexamples/09_main_window/02_status_bar.py
+++ src/qtwidgetsexamples/09_main_window/02_status_bar.py
@@ -16,16 +16,19 @@ class QEditor(QMainWindow):
         self.text_edit = QTextEdit()        
         self.setCentralWidget(self.text_edit)
         
-        # 1 - Create a widget to display information
+        # 1 - Create widgets to display information
         
-        self.label = QLabel()
+        self.position_label = QLabel()
+        self.charcount_label = QLabel()
         
         # 2 - Add the widget to QStatusBar
         #     You access the status bar using QMainWindow.statusBar()
         
-        self.statusBar().addWidget(self.label)
+        self.statusBar().addWidget(self.position_label)
+        self.statusBar().addPermanentWidget(self.charcount_label)
         
         self.text_edit.textChanged.connect(self.on_text_changed)
+        self.text_edit.copyAvailable.connect(self.on_copy_available)
     
     # 3 - Display some stats in the widget associated
     #     with the status bar.
@@ -38,7 +41,11 @@ class QEditor(QMainWindow):
         x = str(cursor.blockNumber() + 1)
         y = str(cursor.columnNumber() + 1)
         
-        self.label.setText('Chars: {}, Ln: {}, Col: {}'.format(size, x, y))
+        self.position_label.setText('Ln: {}, Col: {}'.format(x, y))
+        self.charcount_label.setText('Chars: {}'.format(size))        
+
+    def on_copy_available(self):
+        self.statusBar().showMessage('Copy available!', 2000)
 
 
 if __name__ == '__main__':
