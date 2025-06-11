beddd9f76dc49924de15e07db1b4a1dbd18fb5c5
JoeAnonimist <anonimista@outlook.com>
2025-06-11 08:51:46 +0200

    add code to show qml messages in ide on windows

--- src/qtquickexamples/08_containers/02_scroll_view.py
+++ src/qtquickexamples/08_containers/02_scroll_view.py
@@ -1,10 +1,29 @@
+import os
 import sys
 
+from PySide6.QtCore import QtMsgType, qInstallMessageHandler
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
 
 
+def qt_message_handler(mode, context, message):
+    if mode == QtMsgType.QtInfoMsg:
+        mode = 'Info'
+    elif mode == QtMsgType.QtWarningMsg:
+        mode = 'Warning'
+    elif mode == QtMsgType.QtCriticalMsg:
+        mode = 'Critical'
+    elif mode == QtMsgType.QtFatalMsg:
+        mode = 'Fatal'
+        print(f"{mode}: {message} ({context.file}:{context.line})", file=sys.stderr)
+        sys.exit(1)
+    else:
+        mode = 'Debug'
+    print(f"{mode}: {message} ({context.file}:{context.line})", file=sys.stderr)
+
 if __name__ == '__main__':
+
+    qInstallMessageHandler(qt_message_handler)
 
     app = QGuiApplication(sys.argv)
     
