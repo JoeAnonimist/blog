f5cd2f84efa56c6863832a0bac747ec18b712e93
JoeAnonimist <anonimista@outlook.com>
2025-08-25 09:44:32 +0200

    reimplement qabstractitemmodel sort method

---
+++ src/qtwidgetsexamples/12_model_view_programming/05_sorting_and_filtering/01_sort_method_in_place.py
@@ -0,0 +1,110 @@
+import sys
+
+from PySide6.QtCore import QAbstractTableModel, QModelIndex, Qt
+from PySide6.QtWidgets import (QApplication, QWidget,
+    QTableView, QVBoxLayout)
+from PySide6.QtTest import QAbstractItemModelTester
+
+
+class CsvModel(QAbstractTableModel):
+    
+    def __init__(self, parent=None):
+        
+        super().__init__(parent)
+        
+        self.header = ['Indicator', 'Value (%)', 
+            'Aggregate', 'Include in report']
+        self.csv_data = [
+            ['GDP', 3, 1, True],
+            ['CPI', 6, 1, True],
+            ['Jobs', 5, 0, True],
+            ['Confidence', 75, 0, True],
+            ['Industry', 92, 0, True],
+            ['Retail', 4, 1, True],
+        ]
+
+    def rowCount(self, parent=QModelIndex()):
+        return len(self.csv_data)
+    
+    def columnCount(self, parent=QModelIndex()):
+        return len(self.header)
+    
+    def data(self, index, role):
+        
+        value = self.csv_data[index.row()][index.column()]
+
+        if role == Qt.ItemDataRole.DisplayRole:
+            return value
+        if role == Qt.ItemDataRole.EditRole:
+            if index.column() == 2:
+                return value == 1
+            else:
+                return value
+    
+    def setData(self, index, value, role):
+        
+        if role == Qt.ItemDataRole.EditRole:
+            if index.column() == 2:
+                value = 1 if value else 0
+            if self.csv_data[index.row()][index.column()] != value:
+                self.csv_data[index.row()][index.column()] = value
+                self.dataChanged.emit(index, index)
+                return True
+            return False
+        return False
+
+    def sort(self, column, order):
+        
+        self.layoutAboutToBeChanged.emit()
+        self.csv_data.sort(
+            key=lambda row: row[column],
+            reverse=(order == Qt.SortOrder.DescendingOrder)
+        )
+        self.layoutChanged.emit()
+        
+        for row in self.csv_data:
+            print(row)
+
+    
+    def flags(self, index):
+        flags = Qt.ItemFlags.ItemIsSelectable | \
+            Qt.ItemFlags.ItemIsEnabled | \
+            Qt.ItemFlags.ItemIsEditable
+        return flags
+
+    def headerData(self, section, orientation, role):
+        if orientation == Qt.Orientation.Horizontal:
+            if role == Qt.ItemDataRole.DisplayRole:
+                return self.header[section]
+
+
+class Window(QWidget):
+    
+    def __init__(self):
+
+        super().__init__()
+
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+
+        model = CsvModel()
+        QAbstractItemModelTester(model)
+        
+        view = QTableView()
+        view.setModel(model)
+        
+        view.setSortingEnabled(True)
+        
+        view.resizeColumnsToContents()
+        layout.addWidget(view)
+
+
+if __name__ == '__main__':
+
+    app = QApplication(sys.argv)
+
+    main_window = Window()
+    main_window.show()
+
+    sys.exit(app.exec())
+
