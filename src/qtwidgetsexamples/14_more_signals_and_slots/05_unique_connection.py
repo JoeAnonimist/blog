6db0e7d537590c701e16cbd5a603304095b5fa85
Joe <anonimista@outlook.com>
2025-09-02 18:29:22 +0200

    unique connection

--- src/qtwidgetsexamples/14_more_signals_and_slots/05_unique_connection.py
+++ src/qtwidgetsexamples/14_more_signals_and_slots/05_unique_connection.py
@@ -13,52 +13,49 @@ class Window(QWidget):
         layout = QVBoxLayout()
         self.setLayout(layout)
         
-        self.checkbox_1 = QPushButton('Checkbox 1: 0 receivers')
-        self.checkbox_1.setCheckable(True)
+        self.button = QPushButton('Click Me')        
+        self.label = QLabel('Button: 0 receivers')
         
-        self.label = QLabel()
-        
-        layout.addWidget(self.checkbox_1)
+        layout.addWidget(self.button)
         layout.addWidget(self.label)
 
         h_layout = QHBoxLayout()
-        self.button_connect = QPushButton('Connect signals')
-        self.button_disconnect = QPushButton('Disconnect signals')
+        self.connect_btn = QPushButton('Connect signals')
+        self.disconnect_btn = QPushButton('Disconnect signals')
         
-        self.button_connect.clicked.connect(self.on_connect_button_clicked)
-        self.button_disconnect.clicked.connect(self.on_disconnect_button_clicked)
+        self.connect_btn.clicked.connect(
+            self.on_connect_btn_clicked)
+        self.disconnect_btn.clicked.connect(
+            self.on_disconnect_btn_clicked)
         
-        h_layout.addWidget(self.button_connect)
-        h_layout.addWidget(self.button_disconnect)
+        h_layout.addWidget(self.connect_btn)
+        h_layout.addWidget(self.disconnect_btn)
         layout.addLayout(h_layout)
 
-    def on_connect_button_clicked(self):
-        
-        self.checkbox_1.clicked.connect(self.on_state_changed)
-        
-        self.update_checkbox_texts()
+    def on_connect_btn_clicked(self):
+        self.button.clicked.connect(self.on_clicked,
+            Qt.ConnectionType.UniqueConnection)        
+        self.update_label()
 
-    def on_disconnect_button_clicked(self):
-
-        self.checkbox_1.clicked.disconnect(self.on_state_changed)
-
-        self.update_checkbox_texts()
+    def on_disconnect_btn_clicked(self):
+        self.button.clicked.disconnect(self.on_clicked)
+        self.update_label()
         
     @Slot(bool)
-    def on_state_changed(self, checked):
-        sender_name = self.sender().objectName()
-        self.label.setText(f'{sender_name} {checked}')
+    def on_clicked(self, checked):
+        print('Button clicked')
         
-    def update_checkbox_texts(self):
-
-        self.checkbox_1.setText('Checkbox 1: ' +
-            str(self.receiver_count(self.checkbox_1)) + ' receivers')
+    def update_label(self):
+        self.label.setText('Button: ' +
+            str(self.receiver_count(self.button)) + 
+            ' receivers')
         
-    def receiver_count(self, checkbox):
-        return checkbox.receivers(SIGNAL('clicked(bool)'))
+    def receiver_count(self, button):
+        return button.receivers(SIGNAL('clicked(bool)'))
 
 
 if __name__ == '__main__':
+    
     app = QApplication(sys.argv)
     main_window = Window()
     main_window.show()
