d78a98c2a77cfefb162a365208edc2af9b95ea05
JoeAnonimist <anonimista@outlook.com>
2025-09-02 08:21:59 +0200

    slot execution order

---
+++ src/qtwidgetsexamples/14_more_signals_and_slots/02_multiple slots.py
@@ -0,0 +1,42 @@
+import sys
+from PySide6.QtWidgets import (QApplication, 
+    QWidget, QPushButton, QVBoxLayout, )
+
+
+class Window(QWidget):
+    
+    def __init__(self):
+
+        super().__init__()
+        
+        layout = QVBoxLayout()
+        self.setLayout(layout)
+        
+        self.button = QPushButton('Click me!')
+        layout.addWidget(self.button)
+        
+        self.conn = self.button.clicked.connect(
+            self.on_button_clicked_1)
+        self.conn = self.button.clicked.connect(
+            self.on_button_clicked_2)
+        self.conn = self.button.clicked.connect(
+            self.on_button_clicked_3)
+    
+    def on_button_clicked_1(self):
+        print('Executed first')
+        
+    def on_button_clicked_2(self):
+        print('Executed second')
+        
+    def on_button_clicked_3(self):
+        print('Executed third')
+
+
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+
+    main_window = Window()
+    main_window.show()
+    
+    sys.exit(app.exec())
