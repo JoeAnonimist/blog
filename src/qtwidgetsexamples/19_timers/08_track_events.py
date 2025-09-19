1fcc63a3db99ae569171b12dd775331cc886aa6d
JoeAnonimist <anonimista@outlook.com>
2025-09-19 08:38:12 +0200

    track events

--- src/qtwidgetsexamples/19_timers/08_track_events.py
+++ src/qtwidgetsexamples/19_timers/08_track_events.py
@@ -1,8 +1,48 @@
 import sys
-from PySide6.QtCore import QTimer, Qt
+import time
+from PySide6.QtCore import QObject, QTimer, Qt
 from PySide6.QtGui import QInputEvent
 from PySide6.QtWidgets import (QApplication, 
     QWidget, QLabel, QVBoxLayout)
+
+
+class IdleFilter(QObject):
+    
+    def __init__(self, parent, window):
+        
+        super().__init__(parent)
+
+        self.window = window
+        self.is_idle = False
+        self.last_ui_update = 0
+        
+        self.timer = QTimer()
+        self.timer.setInterval(3000)
+        self.timer.timeout.connect(self.on_timeout)
+        self.timer.start()
+        
+    def on_timeout(self):
+        
+        print('timeout')
+        self.window.label.setStyleSheet('background: lightgrey;')
+        self.window.label.setText('Sleeping...')
+        self.window.setWindowOpacity(0.8)
+        self.is_idle = True
+
+    def eventFilter(self, obj, event):
+        
+        if isinstance(event, QInputEvent):
+            
+            current_time = time.time()
+
+            if self.is_idle and (current_time - self.last_ui_update >= 0.1):
+                self.window.label.setStyleSheet('border: 1px solid grey;')
+                self.window.label.setText('Stop user activity to sleep')
+                self.window.setWindowOpacity(1.0)
+                self.is_idle = False
+                self.last_ui_update = current_time
+            self.timer.start() # restarts timer
+        return super().eventFilter(obj, event)
 
 
 class Window(QWidget):
@@ -14,33 +54,18 @@ class Window(QWidget):
         layout = QVBoxLayout()
         self.setLayout(layout)
         self.resize(300, 200)
-        # self.setMouseTracking(True)
+        self.setMouseTracking(True)
         
         self.label = QLabel('Stop user activity to sleep')
         self.label.setStyleSheet('border: 1px solid grey;')
         self.label.setAlignment(
             Qt.AlignmentFlag.AlignHCenter |
             Qt.AlignmentFlag.AlignVCenter)
-        # self.label.setMouseTracking(True)
+        self.label.setMouseTracking(True)
         layout.addWidget(self.label)
-        
-        self.timer = QTimer()
-        self.timer.setInterval(3000)
-        self.timer.timeout.connect(self.on_timeout)
-        self.timer.start()
-        
-    def on_timeout(self):
-        print('timeout')
-        self.label.setStyleSheet('background: lightgrey;')
-        self.label.setText('Sleeping...')
-        
-    def event(self, event):
-        if isinstance(event, QInputEvent):
-            print(event)
-            self.label.setStyleSheet('border: 1px solid grey;')
-            self.label.setText('Stop user activity to sleep')
-            self.timer.start()
-        return super().event(event)
+
+        self.idle_filter = IdleFilter(self, self)
+        QApplication.instance().installEventFilter(self.idle_filter)
 
 
 if __name__ == '__main__':
