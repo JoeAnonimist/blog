baf7408e1b1d633d5999171b7d1eed1d1f83dd8f
Joe <anonimista@outlook.com>
2025-10-24 19:00:57 +0200

    easing types

--- src/qtquickexamples/03_qt_quick/07_animations_and_behaviors/09_easing_curves/main.py
+++ src/qtquickexamples/03_qt_quick/07_animations_and_behaviors/09_easing_curves/main.py
@@ -1,4 +1,5 @@
 import sys
+from PySide6.QtCore import QEasingCurve
 from PySide6.QtGui import QGuiApplication
 from PySide6.QtQml import QQmlApplicationEngine
 
@@ -7,8 +8,11 @@ if __name__ == '__main__':
 
     app = QGuiApplication(sys.argv)
 
+    easingTypes = [t.name for t in QEasingCurve.Type]
+
     engine = QQmlApplicationEngine()
     engine.quit.connect(app.quit)
+    engine.setInitialProperties({'easingTypes': easingTypes})
     engine.load('Main.qml')
 
     sys.exit(app.exec())
